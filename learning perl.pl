62b4edfe6282b26b4804f4e2e9c268901ce5ded5
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-11-04 11:47:43 -0500

    Feat: add arrays and stacks

--- learning perl.pl
+++ learning perl.pl
@@ -198,8 +198,111 @@ sub fun_with_complex_list{
   print(('salt', 'vinegar', 'mustard', 'pepper')[2], "\n");
 
   #List Slices
-  
+  print((19, 68, 47, 60, 53, 51, 58, 55, 47)[(4,5,6)], "\n");
+  print((19, 68, 47, 60, 53, 51, 58, 55, 47)[4,5,6], "\n");
 
+  my $first; my $second;
+  ($first, $second) = (1,3);
+  print (("heads ", "shoulders ", "knees ", "toes ")[$first, $second], "\n");
+
+  #trying something fun
+  ($first, $second) = ($second, $first); #this switches the place
+  print (("heads ", "shoulders ", "knees ", "toes ")[$first, $second], "\n");
 
 }
+
 &fun_with_complex_list;
+
+=begin comment
+Using range
+=end comment
+=cut
+sub fun_with_range{
+  print("Counting up: ", (1..6), "\n");
+  print("Counting down: ", (6..1), "\n");
+  print("Counting down the right way: ", reverse(1..6), "\n");
+
+  #this also works with alphabets
+  print "alphabets: ", ('a'..'m'), "\n";
+  print "alphabets in reverse: ", reverse('n'..'z'), "\n";
+
+  #then how about alphanumeric?
+  print("Going from 3 to z: ", (3..'z'), "\n");
+  #The above will not work as Perl interprets it as
+  #print("Going from 3 to z: ", (3..0), "\n");
+  print "Going from z to 3: ", ('z'..3), "\n";
+}
+&fun_with_range;
+
+
+=begin comment
+Using Arrays
+=end comment
+=cut
+sub fun_with_arrays{
+  my @days;
+  @days = qw(Monday Tuesday Wednesday Thursday Friday Saturday Sunday);
+  print @days, "\n"; #list context
+  #assigning an array to a scalar variable
+  my $scalar = @days; #scalar context
+  #this will print the number of elements in the array
+  print "This is a scalar variable: ", $scalar, "\n";
+  #this will print all the values in the array
+  print "@days\n";
+
+  #adding to an array
+
+  my @workdays = qw(Monday Tuesday Wednesday Thursday Friday Saturday);
+  my @weekends = qw(Saturday Sunday);
+  my @alldays = (@workdays, @weekends);
+  print(@alldays, "\n");
+
+  my @num1 = 1..5;
+  my @num2 = (0, @num1, 6..10);
+  print (@num2, "\n");
+
+  #accessing single elements
+  (my $first, my $second, my $third) = @days; #Declaration in list is allowed. Wow!
+  print("First day is ", $first, "\n"); #Monday
+  print("Second day is ", $second, "\n"); #Tuesday
+  print("Third day is ", $third, "\n"); #Wednesday
+
+  print(@days[6], "\n"); #This prints Sunday
+
+  #accessing multiple elements
+  print(@days[0,2,5..6], "\n");
+
+  #running through Arrays
+  for my $element (@days){
+    print $element, "\n";
+  }
+
+  #Array Function
+  my @count = (1..5);
+  for (reverse(@count)){
+    print "$_...\n";
+    sleep 1;
+  }
+  print "BLAST OFF!\n";
+
+}
+&fun_with_arrays;
+
+=begin comment
+Pop and Push to an array
+=end comment
+=cut
+sub fun_with_stack{
+  #we can use the array as a stack
+  my @coffee = ("Macchiato", "Cappuccino", "Americano", "Latte");
+  @coffee = sort @coffee; #sorts the array
+  print(@coffee, "\n");
+  push @coffee, "Mocha";
+  print(@coffee, "\n");
+  pop(@coffee);
+  pop(@coffee);
+  print(@coffee, "\n");
+
+}
+
+&fun_with_stack;
