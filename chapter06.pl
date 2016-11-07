2182908b6194819e70561526ddcb21f03a9ff892
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-11-07 14:27:47 -0600

    Feat: added Chapter 6

--- chapter06.pl
+++ chapter06.pl
@@ -7,3 +7,58 @@ package Learning::chapter::06;
 Files and Data
 =end comment
 =cut
+
+sub simple_file{
+  print STDOUT "Hello, World.\n";
+  #open FH, $filename or die $!;
+  open FILE, 'README.md' or die "I think we have a problem here $!";
+  my $lineno = 1;
+  while (<FILE>){
+    print $lineno++;
+    print ": $_";
+  }
+
+  #Filters
+  #Run the code as "perl chapter06.pl  chapter05.pl chapter04.pl chapter03.pl" This is very long
+  my $current = "";
+  while (<>){
+    if ($current ne $ARGV){
+      $current = $ARGV;
+      print "\n\t\tFile: $ARGV\n\n";
+      $lineno = 1;
+    }
+    print $lineno++;
+    print ": $_";
+  }
+}
+#&simple_file();
+
+sub write_to_file{
+  #This one will overwrite anything that is in summary.txt
+  #perl chapter06.pl  chapter05.pl chapter04.pl chapter03.pl > summary.txt
+
+  #This one will add to summary.txt
+  #perl chapter06.pl  chapter01-02.pl >> summary.txt
+
+  #For inputting files use
+  # perl chapter06.pl < summary.txt
+
+  #we can also use this in perl
+  #open FH, "< $filename";
+
+  #COPY a file
+  my $source = shift @ARGV;
+  my $destination = shift @ARGV;
+
+  open IN, $source or die "Can't read source file $source: $!\n";
+  open OUT, ">$destination" or die "Can't write on file $destination: $!\n";
+
+  print "Copying $source to $destination\n";
+
+  while (<IN>) {
+    print OUT $_;
+  }
+
+}
+
+&write_to_file(); # "perl chapter06.pl  chapter01-02.pl output.txt"
